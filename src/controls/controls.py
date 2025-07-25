b315ffb4e446176cd064f789911a6ca39ed1517c
ghost59 <as.1714554@gmail.com>
2025-07-25 19:44:00 -0400

    Testing

---
+++ src/controls/controls.py
@@ -0,0 +1,12 @@
+import flet as ft 
+
+class culom(ft.Column):
+    def __init__(self, vote, button):
+        self.vote = vote
+        self.button = button
+    def displays(self):
+        self.controls = []
+        
+
+        
+        
